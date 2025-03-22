dbee64a0709628bc35bc2e16d67da4a636cf7492
uvpatel <uvpatel7271@gmail.com>
2025-03-22 21:17:18 +0530

    Adding file about function this give us overview about parameter arguments and return value.

---
+++ argvspara.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+    Difference between Argument or Parameter.
+    
+    -- funtion which can take value or give value.
+
+    function which takes value it's called parameter and return value.
+    
+    
+    passing Arguments:
+    
+    void printHello();
+    
+    void printTable(int n)*;
+    
+    int sum(int a,int b);
+
+    This sum function is taking two parameter a and b. and return value as int show it's written value is int.
+
+    
+    */
+    return 0;
+}
---
+++ functype.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    /*Library function:
+    
+    Special Function inbuilt in C
+    
+    e.g. scanf(),printf()
+    
+    
+    User Defined
+    
+    Declared & Defined by programmer
+    */
+   
+    return 0;
+}
