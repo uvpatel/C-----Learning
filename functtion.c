ccaf77b1546ad5f7d781a9396a5b0b1b090b3acd
uvpatel <uvpatel7271@gmail.com>
2025-03-22 20:31:30 +0530

    Intro to function

--- functtion.c
+++ functtion.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
 int main(){
+    /*Functions :
+
+    Block of code that performs paticular task.
     
+    Take argument --> Do work --> Return Result.
+
+    it can be used multiple times 
+
+    increase code reusability
+
+    Syntax 1:
+        Function Prototype:
+
+        void printHello();  <-- 
+
+        void meaning is return nothing
+
+    Syntax 2 :
+        Function Definition
+
+        void printHello(){
+        printf("Hello");
+        }
+
+    Syntax 3:
+        
+        Function Call
+
+        int main(){
+             printHello();  <-- we can call function from main otherwise any other function.
+        return 0;
+        }
+
+
+
+    */
     return 0;
 }
