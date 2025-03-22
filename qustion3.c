a6c7116a9272ed0a2b1f87b9b8d495f5f1dfd647
uvpatel <uvpatel7271@gmail.com>
2025-03-22 21:32:20 +0530

    solve qustion 3

---
+++ qustion3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int table(int n);
+int main(){
+    int n;
+    printf("Enter the number you want to Multiplication table: ");
+    scanf("%d",&n);
+    table(n);
+
+
+    return 0;
+}
+
+int table(int n){
+    for (int i = 0; i < 11; i++)
+    {
+        printf("%d X %d = %d\n",n,i,n*i);
+    }
+    
+}
