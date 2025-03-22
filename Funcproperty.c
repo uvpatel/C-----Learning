c58dccfa633ed7149cdde865afc70ac5a5e00743
uvpatel <uvpatel7271@gmail.com>
2025-03-22 21:04:50 +0530

    add some properties of function

---
+++ Funcproperty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    /*
+     - Execution always starts from main
+
+     - A function gets called directly or indirectly from main
+     
+     -There can be multiple function in a program.
+
+    */
+    return 0;
+}
