6e9e964e2a5649b39cedc16d783b7987c38274fe
uvpatel <uvpatel7271@gmail.com>
2025-03-22 20:52:03 +0530

    This i added executio.c this give us overview how execution works in c for functions.

---
+++ execusion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    /*
+        execution starts from main function 
+
+        first execute above function and then call belove funtion it excute then it's belove function.
+    */
+    return 0;
+}
