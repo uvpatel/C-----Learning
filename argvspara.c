3f76603b1bc856ddef2a701dfeffdca7b856f1c6
uvpatel <uvpatel7271@gmail.com>
2025-03-22 21:46:17 +0530

    add more details in argument and parameter

--- argvspara.c
+++ argvspara.c
@@ -22,7 +22,15 @@ int main(){
 
 
     in the function we pass variable which called as a argument like it is always in calling statement and which variable use in function it is known  as knnow as parameter
-    
+
+
+
+
+    Arguments                                |  vs  |          Parameter
+1) values that are passed in function call   |      |      1)values in function declaration and defination
+2)used to send value                         |      |      2) used to recieve value
+3)actual parameter                           |      |      3)formal parameters.
+
     */
     return 0;
 }
