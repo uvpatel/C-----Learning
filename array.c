a48449feb65d2cc4d3f7ad22cc2266e9feb96396
uvpatel <uvpatel7271@gmail.com>
2025-03-23 10:40:25 +0530

    Learning arrays

---
+++ array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    /*Array is Collection of similar data types stored at
+    
+    contiguous memory locations
+
+    why array is useful :
+        if i want 3 integer or 5 float any more than one similar data type.
+        and insted of declaring multiple time we can declare an array /
+        we use array because it's accesabilty. it store in a contiguous memory location. 
+
+        e.g. : 
+           int  marks1 = 97;
+           int  marks2 = 95;
+           int  marks3 = 93;
+
+        //    this is a synatax of an array. 
+        
+           int marks[] = {97,95,93};
+    */
+    
+    return 0;
+}
